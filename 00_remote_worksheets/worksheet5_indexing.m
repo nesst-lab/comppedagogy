8e59bb75880c96655abe380fe08e7b321248ddcd
Robin Karlin <rkarlin@wisc.edu>
2020-03-16 13:41:10 -0500

    description for worksheets 5 6 7

---
+++ 00_remote_worksheets/worksheet5_indexing.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SMNG's BASIC (BLAB And SMAC Intro to Computing) series
+% Remote BASIC series 
+% Worksheet 5: Indexing
+% 
+% - Skills introduced: 
+% --- Indexing introduction
+% --- Indexing on different variable types
+% --- Logical indexing
+% ------ Vowel indices in waverunner
+%
+% Presented by Sarah Bakst and Robin Karlin
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ 00_remote_worksheets/worksheet7_forLoops.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SMNG's BASIC (BLAB And SMAC Intro to Computing) series
+% Remote BASIC series 
+% Worksheet 7: For loops
+% 
+% - Skills introduced: 
+% --- Other ways of defining vectors in Matlab
+% --- For loops
+% --- Toy for loop with sample Audapter data 
+%
+% Presented by Sarah Bakst and Robin Karlin
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
