16863078ac1d174fea2f0234fea854bfec14fe1b
sbakst <bakst.sarah@gmail.com>
2020-03-18 21:50:46 -0500

    wrote intro to idea of indexing

--- 00_remote_worksheets/worksheet6_indexing.m
+++ 00_remote_worksheets/worksheet6_indexing.m
@@ -13,3 +13,9 @@
 % Presented by Sarah Bakst and Robin Karlin
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Suppose you have a spreadsheet in Excel with 20 rows and five columns.
+% Say you want to grab all the information in the 10th row.
+% This means that for every column, you want the tenth item in that column
+% right? In Excel it's easy: you just highlight that row, copy/paste and
+% you're done.
