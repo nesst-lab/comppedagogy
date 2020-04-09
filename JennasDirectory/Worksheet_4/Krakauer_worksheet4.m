322a328af85190f28ab7672ace5aa53edc6b1c59
Jenna Krakauer <jennakrakauer@Jennas-MacBook-Pro.local>
2020-04-09 13:20:59 -0500

    added worksheet 4

---
+++ JennasDirectory/Worksheet_4/Krakauer_worksheet4.m
@@ -0,0 +1,21 @@
+3.
+if isempty(emptyVariable)
+        emptyVariable = 1;
+end
+
+4.
+Var1 = 'SMAC'
+if Var1 = 'SMAC'
+        fprintf('Your PI is Prof. Ben Parrell')
+elseif Var1 = 'BLAB'
+        fprintf('Your PI is Prof. Carrie Niziolek')
+else
+        fprintf('Oh no! You have no PI')
+end
+
+5.
+hypotenuse = sqrt(leg1^2 + leg2^2); % the hypotenuse is equal to the square root of leg1 squared plus leg 2 squared
+
+if round(hypotenuse) ~= hypotenuse % if we round hypotenuse to the nearest integer and it is not equal to the original value of hypotenuse, then (next line)
+    hypotenuse = NaN; % assign hypotenuse to not a number
+    fprintf('No Pythagorean triple exists with these two leg lengths\n'); % prints this script if the above is true
