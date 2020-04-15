8561c46ddcc9bab1b4f2b9caa797688ca149226c
Robin Karlin <rkarlin@wisc.edu>
2020-04-15 11:18:06 -0500

    comments from RK to JK on homework 4

--- JennasDirectory/Worksheet_4/Krakauer_worksheet4.m
+++ JennasDirectory/Worksheet_4/Krakauer_worksheet4.m
@@ -6,6 +6,7 @@ end
 4.
 Var1 = 'SMAC' % RK: don't forget to suppress your output! 
 if Var1 = 'SMAC' % RK: careful of the difference between = and ==! (Also: strcmp is more retro-compatible) 
+    % RK 4/15: still need to fix this
         fprintf('Your PI is Prof. Ben Parrell')
 elseif Var1 = 'BLAB'
         fprintf('Your PI is Prof. Carrie Niziolek')
@@ -19,7 +20,5 @@ hypotenuse = sqrt(leg1^2 + leg2^2); % the hypotenuse is equal to the square root
 if round(hypotenuse) ~= hypotenuse % if we round hypotenuse to the nearest integer and it is not equal to the original value of hypotenuse, then (next line)
     hypotenuse = NaN; % assign hypotenuse to not a number
     fprintf('No Pythagorean triple exists with these two leg lengths\n'); % prints this script if the above is true
-end
-=======
 end % RK: Your lack of end statement was killing me ^^
->>>>>>> 76cf11572a26d3715140b30b8fae8ede12743764
+
