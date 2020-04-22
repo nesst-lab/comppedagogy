61e0fbccb602fc796c731f911805cc3d67919dfc
Sophie Johnson <sajohnson37@wisc.edu>
2020-04-22 12:35:52 -0500

    wk6

---
+++ SophieDirectory/Worksheet_6/JOHNSON_homework6.m
@@ -0,0 +1,14 @@
+%% Item 1:
+% ans1 = myLongVector([200 504 830]);
+%% Item 2:
+% carrieLogicVector = contains(myCells,'carrie');
+% carrieLocation = find(carrieLogicVector);
+% benLogicVector = contains(myCells,'ben');
+% benLocation = find(benLogicVector);
+% [alexaLogicVector] = contains(myCells,'alexa');
+% alexas = myCells(contains(myCells,'alexa')); 
+% isAlexaHere = ~isempty(alexas);
+%% Item 3:
+% a. ehVector contains a list of all the times and positions where "eh" was
+% said in the experiment
+% b. waverunner([ehVector])
