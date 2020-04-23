a635ccb647ca4fcf0d8860e1c37519ef2f2fdead
Jenna Krakauer <jennakrakauer@Jennas-MacBook-Pro.local>
2020-04-23 11:48:04 -0500

    added Worksheet 4s

--- JennasDirectory/Worksheet_4s/Krakauer_worksheet4s.m
+++ JennasDirectory/Worksheet_4s/Krakauer_worksheet4s.m
@@ -86,5 +86,50 @@ end
 fprintf('You may have at most %d events\n',max_events);
 
 2) If a were 4, nothing would print because a does not match either of the cases.
+Switch/case statements are better because they're more condensed. However, they only operate over the expected values of a variable rather than logical statements, so you need to be specific. If/else is better because you can use logical statements.
 
+%1. Switch/case to else/if statement:
+condWord = 'tapper';
+if strcmp(condWord,'capper') || strcmp(condWord,'gapper')
+	max_events = 7;
+elseif strcmp(condWord,'sapper') || strcmp(condWord,'zapper')
+	max_events = 6;
+elseif strcmp(condWord,'sea') || strcmp(condWord,'C') || strcmp(condWord,'Z') || strcmp(condWord,'czar') || strcmp(condWord,'gar') || strcmp(condWord,'cod') || strcmp(condWord,'god')
+	max_events = 5;
+else
+	max_events = NaN;
+end
+fprintf('You may have at most %d events\n',max_events);
 
+%2. else/if to switch/case statement:
+name = 'sarah';
+switch name
+	case 'sarah'
+		almaMater = 'UC Berkeley'
+	case 'robin'
+		almaMater = 'Cornell University'
+end
+fprintf('%s earned her PhD at %s\n', name,almaMater);
+
+end
+
+%3. Can you translate the following to a switch/case statement? Why/why not?Yes because you can add "otherwise" to the switch/case statement for the unknown alma mater.
+name = 'ben';
+switch name
+	case 'sarah'
+		almaMater = 'UC Berkeley';
+		possPro = 'her';
+	case 'robin'
+		almaMater = 'Cornell University';
+		possPro = 'her';
+	case 'ben'
+		almaMater = 'USC';
+		possPro = 'his';
+	case 'carrie'
+		almaMater = 'MIT';
+		possPro = 'her';
+	otherwise
+		almaMater = 'unknown';
+		possPro = 'their';
+end
+fprintf('%s earned %s PhD at %s\n',name,possPro,almaMater);
