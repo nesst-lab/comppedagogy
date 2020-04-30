748d460593361f599bd4cbc561a1b34076230811
Jenna Krakauer <jennakrakauer@Jennas-MacBook-Pro.local>
2020-04-30 13:59:41 -0500

    added worksheet 8, not done yet

---
+++ JennasDirectory/Worksheet_8/Krakauer_worksheet8.m
@@ -0,0 +1,10 @@
+1.for integer 40:50
+      square = integer^2;
+      fprintf('The square of %d is %d\n',integer,square);
+  end
+
+2. for integer 1:2:1000
+       square = integer^2;
+       fprintf('The square of %d is %d\n',integer,square);
+   end
+
