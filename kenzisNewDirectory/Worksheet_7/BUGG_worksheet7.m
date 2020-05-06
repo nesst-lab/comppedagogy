eb75fc1861c5f64c99cb1c4f0188bee71972d6cf
sbakst <bakst.sarah@gmail.com>
2020-05-06 12:49:40 -0500

    see comment

--- kenzisNewDirectory/Worksheet_7/BUGG_worksheet7.m
+++ kenzisNewDirectory/Worksheet_7/BUGG_worksheet7.m
@@ -6,6 +6,9 @@ sarahsLastYear= smngLabMembers(2).smngYears(end) %The answer I got is 2020! Yay!
 %The second way would be to go through 1, 2, 3 etc until you reach the last
 %year. 
 
+SB: Can you think of another way using the 'length' function in just one line? This is a little tricksy.
+
+
 3. ?
 
 4. ?
