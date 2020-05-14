838d01925250e8f6d031a1f23947f2fa97317026
Jenna Krakauer <jennakrakauer@Jennas-MacBook-Pro.local>
2020-05-14 16:05:37 -0500

    worksheet 9

---
+++ JennasDirectory/Worksheet_9/Krakauer_worksheet9.m
@@ -0,0 +1,29 @@
+1.
+fprintf('1 little piggy\n')
+i = 2;
+while i <= 10
+	fprintf('%d little piggies\n',i)
+	i = i + 1;
+end
+
+2.
+aNumber = 0;
+count = 0;
+while round(aNumber) < 1 && count < 101
+	aNumber = -rand;
+	count = count + 1;
+end
+fprintf('This while loop ran %d time(s)\n',count);
+
+3.
+aNumber = 0;
+count = 0;
+while round(aNumber) < 1
+	aNumber = -rand;
+	count = count + 1;
+	if count > 100
+		aNumber = 1;
+	end
+end
+fprintf('This while loop ran %d time(s)\n',count);
+
