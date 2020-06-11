f6e4f38cab4a63dd61281f81cc08471ef6bc7ff1
hantzsch <lhantzsch@wisc.edu>
2020-06-11 11:35:39 -0500

    worksheet 6 practice

---
+++ LanasDirectory/Worksheet_6/HANTZSCH_worksheet6.m
@@ -0,0 +1,26 @@
+%% HW1
+myLongVector(200) %736
+myLongVector(504) %295
+myLongVector(830) %130
+
+%% HW2
+
+carrie = contains(myCells, 'carrie')
+find(carrie)
+%carrie is at index 415
+
+ben = contains(myCells, 'ben')
+find(ben)
+% ben is at index 544
+
+alexa = contains(myCells,'alexa')
+numAlexas = sum(alexa)
+% there are no alexas
+
+%% HW 3
+
+%a. This vector contains the index numbers of all trials conducted with an
+%"eh" vowel
+
+%b. You should put ehVector in the input slot for trial indices
+
