5e4ff06fd2f69af8e3c3f52f23f30216ac6a4c81
Mackenzie Bugg <mackenziebugg@Mackenzies-MacBook-Air.local>
2020-10-05 11:40:59 -0500

    complete

---
+++ kenzisNewDirectory/Worksheet_8/BUGG_worksheet8.m
@@ -0,0 +1,30 @@
+%#1
+for integer = 40:50 
+    square = integer^2; 
+    fprintf('The square of %d is %d\n',integer,square);     
+end
+
+%#2
+oddBelow1000 = [1:2:1000];
+end
+
+%#3
+for trialIx = 1:length(data)
+    trialFileName = [num2str(1) '.mat'];
+    fprintf('The file name for trial %d will be %s\n',trialIx,trialFileName);
+end
+
+%#4
+charIx = [1 4 12 18];
+for Ix = 1:length(names)
+    charIx = goodEvilAvailable(i)
+    officeCharacters(Ix).goodEvil = goodEvil{i}; 
+end
+
+%#5
+trials2run = 31:60
+for wordNumber = 1:length(words4trials)
+    trialNumber = trials2run(wordNumber)
+    words = words4trials(wordNumber)
+    fprintf('The word for trial %d is %s\n',trialNumber,words) %)
+end
