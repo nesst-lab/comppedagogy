bb3395b5d58042de060514f35fc5d7d2e772b010
jkrakauer <jkrakauer@wisc.edu>
2020-10-15 15:53:21 -0500

    worksheet 8

--- JennasDirectory/Worksheet_8/Krakauer_worksheet8.m
+++ JennasDirectory/Worksheet_8/Krakauer_worksheet8.m
@@ -1,35 +1,23 @@
-1.for integer 40:50
-      square = integer^2;
-      fprintf('The square of %d is %d\n',integer,square);
-  end
-
-2. for integer 1:2:1000
+1. for integer = 40:50
        square = integer^2;
        fprintf('The square of %d is %d\n',integer,square);
    end
+ 
+2. oddNumbers = [1:2:1000]
 
-3. trialIx = length(data);
-   for trial = 1:trialIX
-	trialFileName = [num2str(1)'.mat'];
-	fprintf('The file name for trial %d will be %s\n',trialIx,trialFileName);
+3. ntrials = length(data);
+ for trialIX = 1:ntrials
+     trialFileName = [num2str(1) '.mat'];
+     fprintf('The file name for trial %d will be %s\n',trialIX,trialFileName);
+ end
+ 
+4. for i = 1:length(goodEvilAvailable)
+       charIx = goodEvilAssignment(i);
+       officeCharacters(charIx).goodEvil = goodEvil{i};
    end
 
-4. assignGoodEvil = [1 4 12 18];
-   for charIx = assignGoodEvil
-	officeCharacters(charIx).goodEvil = goodEvil{i};
+5. ntrials = length(words4trials);
+   for i = 1:ntrials
+       word = words4trials(i);
+       fprintf('The word for trial %d is %s\n',i,word);
    end
-
-or
-
-for i = 1:length(goodEvilAvailable)
-	charIx = goodEvilAvailable(i);
-	officeCharacters(charIx).goodEvil = goodEvil{i};
-end
-
-5. traisl2run = 31:60;
-   for i = 1:length(words4trials)
-	trialNo = trials2run(i);
-	trialWord = words4trials{i};
-	fprintf('The word for trial %d is %s\n',trialNo,trialWord)
-   end
-
