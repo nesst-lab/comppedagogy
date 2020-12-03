f28fd048260ad46f6aee37312980f39a1cccb302
brendenmock <60893057+brendenmock@users.noreply.github.com>
2020-12-03 12:27:25 -0600

    Worksheet 10

---
+++ BrendensDir/Worksheet_10/MOCK_worksheet10.m
@@ -0,0 +1,15 @@
+%%HW Item 1:
+% This is applying all the parameters from itrial to all trials.
+
+%%HW Item 2:
+% When itrial is equal to the specified trial that includes a break.
+
+%%HW Item 3:
+% It references the list of words that are contained within the cell
+% matrix.
+
+%%HW Item 4:
+expt.listNoise = listNoise:
+noise2play = expt.listNoise{itrial};
+%%HW Item 4:
+% It creates all of the expt values that are eventually used by audapter.
