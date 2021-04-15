256054b7006fcb32d2b4e9fc8da20e57211cb47b
Lily Englebert <lilyenglebert@Lilys-MacBook-Air-2.local>
2021-04-15 16:54:42 -0500

    worksheet 7

---
+++ lilysNewDirectory/Worksheet_7/ENGLEBERT_homework7.m
@@ -0,0 +1,21 @@
+%homework 1
+smngLabMembers(2).name = 'Sarah Bakst'; 
+smngLabMembers(2).lab = 'BLAB';
+smngLabMembers(2).phdYear = 2018;
+smngLabMembers(2).smngYears = 2017:2020;
+
+%homework 2
+smngLabMembers(2).smngYears(end)
+
+%homework 3
+cellArray= expt.colorvals
+doubleArray= expt.allWords
+
+%homework 4
+formants = data(15).fmts; 
+formant2 = formants(:,2); 
+
+formant3 = data(30).fmts(:,3); 
+
+%homework 5
+newStruct= struct('letters',{'a','b','c'}, 'numbers', {1 2 3}, 'vectors', {[4 5 6] [7 8 9] [10 11 12]}, 'colors',{'red' 'pink' 'blue'})
