4fb44a3c98f61cdec22679bb60764d560ac99f1b
Lily Englebert <lilyenglebert@Lilys-MacBook-Air-2.local>
2021-04-21 15:20:43 -0500

    worksheet 4s and 8

---
+++ lilysNewDirectory/Worksheet_8/ENGLEBERT_homework8.m
@@ -0,0 +1,19 @@
+%homework 1
+for integer = 40:50
+square= integer^2;
+fprintf('the square of %d is %d\n', integer, square)
+end
+
+%homework 2
+oddBelow1000= [1:2:1000]
+
+
+% Homework item 3: 
+
+trialIx=1;
+for trialIx=1
+    trialFileName = [num2str(1) '.mat'];
+    fprintf('The file name for trial %d will be %s\n',trialIx,trialFileName); 
+end
+
+
