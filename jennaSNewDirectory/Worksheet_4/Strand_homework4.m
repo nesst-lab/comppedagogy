bc80e27e4bd0d00dc493408f874ddc3f4ddd583c
Jenna Strand <jennastrand@Jennas-MacBook-Pro.local>
2021-04-26 13:36:07 -0500

    homework 4

---
+++ jennaSNewDirectory/Worksheet_4/Strand_homework4.m
@@ -0,0 +1,24 @@
+Homework 3: if isempty(varEnter)
+	varEnter = 3;
+	end
+
+Homework 4: if strcmp(variable, 'SMAC')
+	fprintf('Your PI is Prof. Ben Parrell\n')
+
+else if strcmp(variable, 'BLAB')
+	fprintf('Your PI is Prof. Carrie Niziolek\n')
+else
+	fprintf('Oh no! You have no PI\n')
+end
+
+Homework 5: hypotenuse = sqrt(leg1^2 + leg2^2);
+This finds the hypotenuse by calculating the square root of leg1^2 + leg2^2.
+
+if round(hypotenuse) ~= hypotenuse
+This rounds the value of the hypotenuse to a whole number.
+
+hypotenuse = NaN;
+This sets the hypotenuse as the not a number value.
+
+fprintf('No Pythagorean triple exists with these two leg lengths\n');
+This will print 'No Pythagorean triple exists with these two leg lengths'.
