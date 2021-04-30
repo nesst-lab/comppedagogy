0397b2ce1d4c4520a76af40fc6d669615f7a570c
Anna Gavin <annagavin@Annas-MacBook-Air-3.local>
2021-04-30 11:33:28 -0500

    finished worksheet 8

---
+++ annasNewDirectory/Worksheet_8/Gavin_worksheet8.m
@@ -0,0 +1,38 @@
+homework 1
+for integer = 40:50 % the definition of the bounds you want to do the operation in
+    square = integer^2; 
+    fprintf('The square of %d is %d\n',integer,square); % the operation!
+end
+
+The square of 40 is 1600
+The square of 41 is 1681
+The square of 42 is 1764
+The square of 43 is 1849
+The square of 44 is 1936
+The square of 45 is 2025
+The square of 46 is 2116
+The square of 47 is 2209
+The square of 48 is 2304
+The square of 49 is 2401
+The square of 50 is 2500
+
+homework 2
+
+oddBelow1000 = [1:1:1000]
+
+homework 3
+
+for trialIx = 1:length(data)
+   trialFileName = [num2str(trialIx) '.mat'];
+   fprintf('The file name for trial %d will be %s\n',trialIx,trialFileName);
+    
+end
+
+homework 4
+
+goodEvilAvailabale = [1 4 12 18];
+i=1;
+for i = 1:length(goodEvilAvailable)
+charIx = goodEvilAvailable 
+
+homework 5
