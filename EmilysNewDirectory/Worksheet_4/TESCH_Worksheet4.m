e8109f1861b44ec2d560858ff0e08c1ed6351bbf
eltesch <eltesch@wisc.edu>
2021-04-30 12:08:17 -0500

    finished worksheet 4 and 4s

--- EmilysNewDirectory/Worksheet_4/TESCH_Worksheet4.m
+++ EmilysNewDirectory/Worksheet_4/TESCH_Worksheet4.m
@@ -20,3 +20,51 @@ if round(hypotenuse) ~= hypotenuse %if the rounded hypotenuse doesn't equal hypo
 end
 
 end
+if a>0 && b>0
+    fprintf('hello world')
+end
+if round(a) == a && round(b)==b
+    fprintf('hello world\n')
+end
+if a~=0 && b~=0
+    fprintf('hello world\n')
+end
+if~isempty(a) && ~isempty(b)
+    fprintf('hello world\n')
+end
+if a>0 || b>0
+    fprintf('hello world\n')
+end
+if round(a)== a || round(b) ==b 
+    fprintf('hello world\n')
+end
+if a~=0 || b~=0
+    fprintf('hello world\n')
+end
+if ~isempty(a) || ~isempty(b)
+    fprintf('hello world\n')
+end
+if xor (a>0,b>0) 
+    fprintf('hello world\n')
+end
+if xor (isempty(a),~isempty(b)) 
+    fprintf('hello world\n')
+end
+if strcmp(ocndWord, 'capper') || strcmp(condWord,'gapper')
+    max_events = 7;
+elseif strcmp(conWord, 'sapper') || strcmp(condWord, 'zapper')
+    max_events=6;
+elseif strcmp(conWord, 'sea')|| strcmp(condWord,'C')||strcmp(condWord,'Z')||strcmp(condWord,'czar')||strcmp(condWord,'gar')||strcmp(condWord, 'cod')||strcmp(condWord, 'god')
+    max_events=5;
+else 
+    max_events=NaN;
+end
+switch name
+    case{'sara'}
+        almaMater='UC Berkeley';
+    case{'robin'}
+        almaMater='Cornell University';
+    otherwise
+        almaMater='???';
+end
+%yes that could be translated in the same way the first two were. 
