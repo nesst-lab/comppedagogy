ffa57e8806f899e42ef99da6228ba99b2dc2d47e
ebaumgardt <ebaumgardt@wisc.edu>
2021-06-18 13:29:31 -0500

    finished worksheet 8

---
+++ EmmasDirectory/worksheet_4/booltest.m
@@ -0,0 +1,8 @@
+function booltest()
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+btestMode = 1;
+if btestMode
+    fprintf('This is a yes\n')
+end
+
---
+++ EmmasDirectory/worksheet_7/BAUMGARDT_worksheet_7.m
@@ -0,0 +1,9 @@
+% Homework Item 5
+field1 = 'letters';
+value1 = {'a'; 'b'; 'c'};
+field2 = 'numbers';
+value2 = {1; 2; 3};
+field3 = 'vectors';
+value3 = {[1 2 3]; [4 5 6]; [7 8 9]};
+structure = struct(field1, value1, field2, value2, field3, value3)
+    
